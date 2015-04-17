8d30226785f4b171c2bb7a4446be522eea0b4428
aitorzubizarret <aitorzubizarreta@yahoo.es>
2015-04-17 16:04:08 +0200

    Cambios

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,28 +1,6 @@
-angular.module('cookingCalendar.controllers', [])
+angular.module('CookingCalendarApp.controller', ['$scope'])
 
-.controller('DashCtrl', function($scope) {})
+.controller('ProductsListCtrl', function($scope, getProducts) {
+  console.log("Hello world");
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  }
-})
-
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
-
-.controller('FriendsCtrl', function($scope, Friends) {
-  $scope.friends = Friends.all();
-})
-
-.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
-  $scope.friend = Friends.get($stateParams.friendId);
-})
-
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
 });
